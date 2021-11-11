be33a5d9dcee63eede0f6f9e92ea89bafc0bee62
nitin <nisingh006@gmail.com>
2021-11-11 00:00:54 +0000

    mutiple ec2

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "my-machine" {
+  count = 4     # Here we are creating identical 4 machines.
+  
+  ami = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "my-machine-${count.index}"
+         }
+}
